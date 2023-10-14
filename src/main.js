23b979cb210d29cecd7a9f1834feddb1472e101d
zmzm <3124140355@qq.com>
2023-10-14 15:29:05 +0800

    feat:新增element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
-import Vuerify from 'vuerify'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(Vuerify)
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
 	render: h => h(App),

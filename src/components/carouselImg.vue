f4a4f88b2049bd1ddfe9b73ad2b13954ff92df32
zmzm <3124140355@qq.com>
2023-11-14 19:36:49 +0800

    feat:新增轮播图组件

--- src/components/carouselImg.vue
+++ src/components/carouselImg.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="swiper-container">
-		<div class="swiper-wrapper">
+		<div class="swiper-wrapper" ref="swiper-wrapper">
 			<div class="swiper-slide" v-for="(img, index) in images" :key="index">
 				<img class="swiper-img" :src="img" alt="img">
 			</div>
@@ -9,28 +9,39 @@
 		<div class="swiper-pagination"></div>
 
 		<!-- 如果需要导航按钮 -->
-		<div class="swiper-button-prev"></div>
-		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev" v-if="browserWidth > 700"></div>
+		<div class="swiper-button-next" v-if="browserWidth > 700"></div>
 	</div>
 </template>
 
 <script>
 import Swiper from 'swiper';
 import 'swiper/css/swiper.min.css';
+import 'viewerjs/dist/viewer.css';
+import Viewer from "viewerjs";
 
 export default {
 	name: 'CarouselImg',
+	data() {
+		return {
+			browserWidth: 0
+		}
+	},
 	props: ['images'],
+	created() {
+		//获取浏览器宽度
+		this.browserWidth = window.innerWidth;
+	},
 	mounted() {
-		let mySwiper = new Swiper('.swiper-container', {
+		new Swiper('.swiper-container', {
 			//循环播放
 			loop: true,
 			//自动播放
-			autoplay: {
+			/* autoplay: {
 				delay: 10000, // 每次轮播间隔的ms数，默认10000ms
 				stopOnLastSlide: false, // 播放完最后一张图片后是否停留在最后一张图片上，停止继续轮播。默认false
 				disableOnInteraction: false, // 用户操作轮播图后，比如点击轮播按钮或小圆点，停止自动轮播
-			},
+			}, */
 			//滚动一张或者切换一张图片，需要的时间，单位ms，默认300ms
 			// speed: 800,
 
@@ -47,20 +58,16 @@ export default {
 			},
 		});
 
-		//鼠标移出隐藏按钮，移入显示按钮
-		mySwiper.el.onmouseover = function () {
-			mySwiper.navigation.$nextEl.removeClass('swiper-button-hide');
-			mySwiper.navigation.$prevEl.removeClass('swiper-button-hide');
+		//移动端
+		if (this.browserWidth <= 700) {
+			new Viewer(this.$refs['swiper-wrapper'], {
+				button: false,
+				navbar: false,
+				title: false,
+				toolbar: false,
+				initialCoverage: 1,
+			});
 		}
-		mySwiper.el.onmouseout = function () {
-			mySwiper.navigation.$nextEl.addClass('swiper-button-hide');
-			mySwiper.navigation.$prevEl.addClass('swiper-button-hide');
-		}
-
-		//获取图片宽高
-		console.log(document.querySelectorAll('.swiper-img')[0].naturalWidth);
-		console.log(document.querySelectorAll('.swiper-img')[1].naturalWidth);
-		console.log(document.querySelectorAll('.swiper-img')[2].naturalWidth);
 	}
 }
 </script>
@@ -69,25 +76,16 @@ export default {
 .swiper-container {
 	width: 100%;
 	height: 100%;
+	background-color: #000000;
 }
 
 .swiper-slide {
 	text-align: center;
-	background-color: #000000;
 }
 
 .swiper-img {
 	max-width: 100%;
 	height: 100%;
 	object-fit: cover;
-}
-
-/*导航显示与隐藏动画*/
-.swiper-button-hide {
-	opacity: 0;
-}
-.swiper-button-prev,
-.swiper-button-next {
-	transition: opacity .6s;
 }
 </style>

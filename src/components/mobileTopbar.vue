08446ee18c5cceb1c4d9b2379b0ce0450585fed7
zmzm <3124140355@qq.com>
2024-01-06 09:31:21 +0800

    feat:新增顶部导航条固定

--- src/components/mobileTopbar.vue
+++ src/components/mobileTopbar.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="mobileTopbar">
-		<div class="icon" @click="clickHandle">
-			<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="rgb(34, 8, 204)" d="M0 88C0 74.7 10.7 64 24 64H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zM448 408c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24H424c13.3 0 24 10.7 24 24z"></path></svg>
+		<div class="box" ref="box">
+			<div class="icon" @click="clickHandle">
+				<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="rgb(34, 8, 204)" d="M0 88C0 74.7 10.7 64 24 64H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zM448 408c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24H424c13.3 0 24 10.7 24 24z"></path></svg>
+			</div>
+			<div class="info">{{userInfo.username}}'s Personal Space</div>
 		</div>
-		<div class="info">{{userInfo.username}}'s Personal Space</div>
 	</div>
 </template>
 
@@ -15,28 +17,65 @@ export default {
 	computed: {
 		...mapState(['userInfo']),
 	},
+	data() {
+		return {
+			oldScrollTop: 0,
+		}
+	},
 	methods: {
 		//点击icon展开侧边栏
 		clickHandle() {
 			this.$store.commit('SIDEBAR_FLAG', true);
+		},
+
+		scrolling() {
+			// 滚动条距文档顶部的距离（做兼容处理）===》不懂的可以结合画图理清逻辑
+			let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+			// 滚动条滚动的距离
+			let scrollStep = scrollTop - this.oldScrollTop;
+			// 更新——滚动前，滚动条距文档顶部的距离
+			this.oldScrollTop = scrollTop;
+			if (scrollStep > 0) {
+				this.$refs['box'].style.top = '-60px';
+			} else {
+				this.$refs['box'].style.top = '0';
+			}
 		}
+	},
+	mounted() {
+		console.log('绑定');
+		window.addEventListener("scroll", this.scrolling);
+	},
+	beforeDestroy() {
+		window.removeEventListener("scroll", this.scrolling);
 	}
 }
 </script>
 
 <style scoped>
 .mobileTopbar {
-	height: 30px;
+	--mobileTopbarHeight: 45px;
+	height: var(--mobileTopbarHeight);
+}
+
+.mobileTopbar .box {
+	width: 100vw;
+	height: var(--mobileTopbarHeight);
 	display: flex;
 	align-items: center;
 	font-size: 1.3rem;
 	font-weight: bold;
-	padding-bottom: 8px;
 	border-bottom: 1px solid rgb(229, 224, 223);
 	margin-bottom: 8px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 5;
+	background-color: #F7F3F2;
+	transition: all .5s;
 }
 
-.mobileTopbar .icon {
+.mobileTopbar .box .icon {
 	height: 100%;
 	width: 10%;
 	display: flex;
@@ -44,14 +83,14 @@ export default {
 	align-items: center;
 }
 
-.mobileTopbar .info {
+.mobileTopbar .box .info {
 	height: 100%;
 	width: 90%;
 	text-align: center;
-	line-height: 30px;
+	line-height: var(--mobileTopbarHeight);
 }
 
-.mobileTopbar .icon svg {
+.mobileTopbar .box .icon svg {
 	width: 18px;
 	height: 18px;
 }

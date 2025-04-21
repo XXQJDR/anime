5607ed33200888b0b1410ad479c3082b417c97ec
zmzm <3124140355@qq.com>
2025-04-21 08:58:36 +0800

    feat: 添加element-ui组件按需引入，优化项目性能

--- src/main.js
+++ src/main.js
@@ -1,14 +1,63 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import store from '@/store';
 import VueLazyLoad from 'vue-lazyload';
 import '@/icons';
 
+import {
+	Backtop,
+	Popover,
+	Tooltip,
+	Message,
+	Loading,
+	Rate,
+	Icon,
+	Select,
+	Option,
+	Divider,
+	Input,
+	Button,
+	Upload,
+	Empty,
+	Skeleton,
+	SkeletonItem,
+	Dialog,
+	MessageBox,
+	Image,
+	Form,
+	FormItem,
+	Steps,
+	Step,
+	Result,
+} from 'element-ui';
+
+Vue.use(Backtop);
+Vue.use(Popover);
+Vue.use(Tooltip);
+Vue.use(Loading.directive);
+Vue.use(Rate);
+Vue.use(Icon);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Divider);
+Vue.use(Input);
+Vue.use(Button);
+Vue.use(Upload);
+Vue.use(Empty);
+Vue.use(Skeleton);
+Vue.use(SkeletonItem);
+Vue.use(Dialog);
+Vue.use(Image);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Result);
+Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
+
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
 Vue.use(VueLazyLoad, {
 	error: require('@/assets/images/error/image-load-error.webp'),
 	loading: require('@/assets/images/loading/image-loading.gif'),
